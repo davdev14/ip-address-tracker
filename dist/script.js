fa095769de9c138882e4c1e50c945c7f22c08c44
davdev14 <105063415+davdev14@users.noreply.github.com>
2022-12-29 17:38:25 +0100

    completed

--- dist/script.js
+++ dist/script.js
@@ -1,21 +1,46 @@
+const address = document.getElementById('ip-address')
+const geoLocation = document.getElementById('location')
+const timeZone = document.getElementById('time-zone')
+const isp = document.getElementById('isp')
+const vpn = document.getElementById('vpn')
+const ipInput = document.getElementById('ip-input')
+const btn = document.getElementById('btn')
+const map = document.getElementById('map')
+const infoBox = document.querySelector('.info-box')
 
 
-
-
-fetch('https://geo.ipify.org/api/v2/country?apiKey=at_GkeWx7tfCgBTKHFmBEnnupvaCcMCs&ipAddress=129.0.76.25')
+const getIpAddressInfo = () => {
+  fetch(`https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_GkeWx7tfCgBTKHFmBEnnupvaCcMCs&ipAddress=${ipInput.value}`)
 .then(Response => Response.json())
 .then(json => {
-  console.log(json.location)
-})
+  address.textContent = json.ip
+  geoLocation.textContent = `${json.location.country}, ${json.location.region}, ${json.location.city}`
+  timeZone.textContent = json.location.timezone
+  isp.textContent = json.isp
+  vpn.textContent = json.proxy.vpn
+  let lat = json.location.lat
+  let lng = json.location.lng
 
-
-
-
-let map = L.map('map').setView([51.5, -0.09], 13);
+  let map = L.map('map').setView(new L.LatLng(lat,lng), 14);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('IP address located')
-    .openPopup();
+let marker = L.marker(new L.LatLng(lat,lng)).addTo(map);
+})
+}
+getIpAddressInfo()
+
+btn.addEventListener('click', () => {
+  getIpAddressInfo()
+})
+
+map.addEventListener('click', () => {
+  map.style.zIndex = 4
+})
+
+window.onclick = (e) => {
+  if (!e.target.matches('#map')) {
+    map.style.zIndex = 1
+  }
+} 
